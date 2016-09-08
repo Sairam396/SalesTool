802454f6256b5c6b006196034610338d4a7d70e4
Sairam Chowdary <sairamchowdary01@gmail.com>
2016-09-08 15:35:33 -0400

    Add a simple output change.

---
+++ SalesToolUsingGitRepos/src/com/salestool/sai/Main.java
@@ -0,0 +1,23 @@
+package com.salestool.sai;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData salesdata = new SalesData();
+		
+		displayGreeting();
+		salesdata.display();
+		
+		
+	}
+		public static void displayGreeting() {
+			System.out.println("HELLO HAPPY SALES PEOPLE!");
+			System.out.println("THIS APP SHOWS SALES DATA");
+			
+			System.out.println("Test 1");
+			
+		}
+
+	}
+
